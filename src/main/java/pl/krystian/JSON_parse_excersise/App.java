21d59b87ad9d490f1ada479b2b83e5405b4b49d9
Krystian <krystian1999@gmail.com>
2020-12-27 20:38:29 +0100

    First commit

---
+++ src/main/java/pl/krystian/JSON_parse_excersise/App.java
@@ -0,0 +1,13 @@
+package pl.krystian.JSON_parse_excersise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
