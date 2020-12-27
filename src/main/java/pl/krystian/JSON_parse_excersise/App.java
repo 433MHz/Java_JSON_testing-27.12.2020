1bb38d701403f042074583de43cbe9079bec9495
Krystian <krystian.izdebski1999@gmail.com>
2020-12-27 20:46:38 +0100

    created simple JSON app to convert Java object int oJSON format

--- src/main/java/pl/krystian/JSON_parse_excersise/App.java
+++ src/main/java/pl/krystian/JSON_parse_excersise/App.java
@@ -1,5 +1,7 @@
 package pl.krystian.JSON_parse_excersise;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Gson g = new Gson();
+    	Employee emp = new Employee();
+    	emp.setFirst_name("Krystian");
+    	emp.setLast_name("Izdebski");
+    	emp.setAge(21);
+    	
+    	String json = g.toJson(emp);
+    	
+    	System.out.println(json);
     }
 }
